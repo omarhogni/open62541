07ac202ba3da1497836464ba6e497956dee53f13
Ómar Högni Guðmarsson <omar@centroid.is>
2025-03-03 09:21:28 +0000

    Try a simpler macos workflow file to begin with

--- plugins/crypto/ua_certificategroup_filestore.c
+++ plugins/crypto/ua_certificategroup_filestore.c
@@ -8,6 +8,8 @@
  */
 
 #include <open62541/util.h>
+#include "../../arch/posix/eventloop_posix.h"
+#include "ua_filestore_common.h"
 #include <open62541/plugin/certificategroup_default.h>
 
 #include "ua_filestore_common.h"
